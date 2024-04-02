9417f8b646eef0dbbb7e550eb23a221e2f8a9e8d
Giedrius <giedrius@blokas.io>
2024-04-02 08:39:37 +0100

    Added -v option

--- args.cpp
+++ args.cpp
@@ -5,14 +5,16 @@
 namespace Args {
   std::string rulesFilePath;
   bool rulesCheckOnly = false;
+  bool versionOnly = false;
 
   bool outputPortDetails = false;
 
   int exitCode = 0;
 
   bool parse(int argc, char* argv[]) {
-    CLI::App app{"bicycle - a MIDI looper"};
+    CLI::App app{"amidiminder - ALSA MIDI connection minder"};
 
+    app.add_flag("-v,--version", versionOnly, "print version and exit");
     app.add_option("-f,--rules-file", rulesFilePath, "file of connection rules");
     app.add_flag("-C,--check-rules", rulesCheckOnly, "check rules file then edit");
     app.add_flag("-p,--port-details", outputPortDetails, "output verbose details of each port");
@@ -22,6 +24,12 @@ namespace Args {
     } catch (const CLI::ParseError &e) {
         exitCode = app.exit(e);
         return false;
+    }
+
+    if (versionOnly)
+    {
+      std::cout << "amidiminder 0.71.0" << std::endl;
+      exit(0);
     }
 
     return true;

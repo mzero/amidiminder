d21c33d263f8cf48d2c65f0fd5670bf90d87d1cf
Mark Lentczner <mark@glyphic.com>
2024-11-25 20:50:35 -0800

    improve help and .1 man page

--- args.cpp
+++ args.cpp
@@ -25,30 +25,43 @@ namespace Args {
     app.add_flag("-v,--verbose", [](auto n){ verbosity = n + 1; }, "Increase level of verbosity");
     app.add_flag("-q,--quiet",   [](auto _){ verbosity = 0; },     "Quiet all normal output");
 
+    const char* userGroup = "User subcommands";
+
     CLI::App *checkApp = app.add_subcommand("check", "Check the syntax of a rules file");
+    checkApp->group(userGroup);
     checkApp->parse_complete_callback([](){ command = Command::Check; });
-    checkApp->add_option("rules-file", rulesFilePath, "Rules file to check")
+    checkApp->add_option("file", rulesFilePath, "Rules file to check; use - for stdin")
+      ->option_text("PATH")
       ->required();
 
-    CLI::App *resetApp = app.add_subcommand("reset", "Reset connections to match the current profile");
-    resetApp->parse_complete_callback([](){ command = Command::Reset; });
-
     CLI::App *loadApp = app.add_subcommand("load", "Load a new porfile, and reset connections");
+    loadApp->group(userGroup);
     loadApp->parse_complete_callback([](){ command = Command::Load; });
-    loadApp->add_option("rules-file", rulesFilePath, "Rules file load")
+    loadApp->add_option("file", rulesFilePath, "Rules file to load; use - for stdin")
+      ->option_text("PATH")
       ->required();
 
     CLI::App *saveApp = app.add_subcommand("save", "Save profile & connections to a rules file");
+    saveApp->group(userGroup);
     saveApp->parse_complete_callback([](){ command = Command::Save; });
-    saveApp->add_option("rules-file", rulesFilePath, "Rules file save to")
+    saveApp->add_option("file", rulesFilePath, "Rules file save to; use - for stdout")
+      ->option_text("PATH")
       ->required();
 
+    CLI::App *resetApp = app.add_subcommand("reset", "Reset connections to match the current profile");
+    resetApp->group(userGroup);
+    resetApp->parse_complete_callback([](){ command = Command::Reset; });
+
+
+    const char* systemGroup = "System commands";
 
     CLI::App *daemonApp = app.add_subcommand("daemon", "Run the minder service");
+    daemonApp->group(systemGroup);
     daemonApp->parse_complete_callback([](){ command = Command::Daemon; });
     daemonApp->add_flag("-p,--port-details", outputPortDetails, "output verbose details of each port");
 
     CLI::App *helpApp = app.add_subcommand("help");
+    helpApp->group(userGroup);
     helpApp->description(app.get_help_ptr()->get_description());
     helpApp->parse_complete_callback([](){ command = Command::Help; });
 

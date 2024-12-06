216efe9513cd76d281117a2e9aa92a25cb3f06c5
Mark Lentczner <mark@glyphic.com>
2024-12-06 09:31:47 -0800

    list --plain

--- src/client.cpp
+++ src/client.cpp
@@ -1,6 +1,8 @@
 #include "client.h"
 
-#include "msg.h"
+#include "fmt/format.h"
+
+#include "args.h"
 #include "seqsnapshot.h"
 
 
@@ -9,17 +11,42 @@ namespace Client {
   void listCommand() {
     SeqSnapshot s;
 
-    Msg::output("Ports:");
-    for (const auto& p : s.ports)
-      Msg::output("    {:{cw}} : {:{pw}} [{:3}:{}] {}",
-        p.client, p.port, p.addr.client, p.addr.port,
-        SeqSnapshot::addressDirStr(p),
-        fmt::arg("cw", s.clientWidth), fmt::arg("pw", s.portWidth));
+    if (!Args::listClients && !Args::listPorts && !Args::listConnections)
+      Args::listPorts = Args::listConnections = true;
 
-    Msg::output("Connections:");
-    for (const auto& c : s.connections) {
-      Msg::output("    {} --> {}", c.sender, c.dest);
+    if (Args::listPorts) {
+      if (Args::listPlain) {
+        for (const auto& p : s.ports)
+          fmt::print("{}:{}\n",
+            p.client, Args::detail() ? p.portLong : p.port);
+            // hidden use of -v in conjunction of --plain
+      }
+      else {
+        std::cout << "Ports:\n";
+        for (const auto& p : s.ports)
+          fmt::print("    {:{cw}} : {:{pw}} [{:3}:{}] {}\n",
+            p.client, p.port, p.addr.client, p.addr.port,
+            SeqSnapshot::addressDirStr(p),
+            fmt::arg("cw", s.clientWidth), fmt::arg("pw", s.portWidth));
+      }
     }
+
+    if (Args::listConnections) {
+      if (Args::listPlain) {
+        for (const auto& c : s.connections)
+          fmt::print("{}:{} --> {}:{}\n",
+            c.sender.client, c.sender.port,
+            c.dest.client, c.dest.port);
+      }
+      else {
+        std::cout << "Connections:\n";
+        for (const auto& c : s.connections) {
+          fmt::print("    {} --> {}\n", c.sender, c.dest);
+        }
+      }
+    }
+
+    std::cout.flush();
   }
 
 }
